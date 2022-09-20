a8f8c1ae09af269bf4d8f067417f78ff2c4cc63d
Ivan <kosseshkaplay@gmail.com>
2022-09-20 20:06:46 +0500

    Нужно больше функций

---
+++ js/main.js
@@ -0,0 +1,13 @@
+function getRandomNumber(min, max) {
+  if (min < 0 || max < 0 || max <= min) {
+      return "Введены не верные значения";
+  }
+  return Math.floor(min + Math.random() * (max + 1 - min));
+}
+
+function checkStringLength(str, maxlegth) {
+  return str.length <= maxlegth;
+}
+
+getRandomNumber(1, 7);
+checkStringLength('я Ваня', 7);
