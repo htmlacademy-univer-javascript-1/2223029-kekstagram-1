06633c57bd2d6597a6ba412d5d5fd2527ea9cf01
Ivan <kosseshkaplay@gmail.com>
2022-09-20 20:43:39 +0500

    Нужно больше функций

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 function getRandomNumber(min, max) {
   if (min < 0 || max < 0 || max <= min) {
-    return 'Введены не верные значения';
+    return 'Введены не верные значения!';
   }
   return Math.floor(min + Math.random() * (max + 1 - min));
 }
