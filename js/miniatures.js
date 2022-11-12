6d21da812b5162265725cafa2be61c48a43dbc1b
Ivan <kosseshkaplay@gmail.com>
2022-11-12 16:59:45 +0500

    Отрисуй меня полностью (часть 1)

---
+++ js/miniatures.js
@@ -0,0 +1,23 @@
+import {arrayOfObjects} from './data.js';
+
+const picturesContainerElement = document.querySelector('.pictures');
+const templateFragment = document.querySelector('#picture').content;
+
+const fragment = document.createDocumentFragment();
+const descriptions = arrayOfObjects();
+
+const createMiniatures = () => {
+  descriptions.forEach((description) => {
+    const photo = templateFragment.cloneNode(true);
+
+    photo.querySelector('.picture__img').src = description.url;
+    photo.querySelector('.picture__likes').textContent = description.likes;
+    photo.querySelector('.picture__comments').textContent = description.comments;
+
+    fragment.appendChild(photo);
+  });
+
+  picturesContainerElement.appendChild(fragment);
+};
+
+createMiniatures();
