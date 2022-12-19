f4942b982fcfc8653f9e3e43c7a1d7cadd6c0204
Keks <keks@htmlacademy.ru>
2022-12-19 16:44:06 +0300

    :heavy_check_mark: Сборка #16

---
+++ 16/js/miniatures.js
@@ -0,0 +1,22 @@
+import { createBigPictures } from './big-photo.js';
+
+const picturesContainer = document.querySelector('.pictures');
+const templateFragment = document.querySelector('#picture').content.querySelector('.picture');
+
+const fragment = document.createDocumentFragment();
+
+const createMiniatures = (descriptions) => {
+  document.querySelectorAll('.picture').forEach((picture) => picture.remove());
+  descriptions.forEach((description) => {
+    const photo = templateFragment.cloneNode(true);
+
+    photo.querySelector('.picture__img').src = description.url;
+    photo.querySelector('.picture__likes').textContent = description.likes;
+    photo.querySelector('.picture__comments').textContent = description.comments.length;
+    fragment.appendChild(photo);
+  });
+  picturesContainer.appendChild(fragment);
+  createBigPictures(descriptions);
+};
+
+export { createMiniatures };
